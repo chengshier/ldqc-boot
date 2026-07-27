b3f35dd54962a9f162d4f8b83af1b1c73f951b16
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-27 18:18:24 +0800

    feat: add training progress mapper

---
+++ src/main/java/org/springblade/modules/trainingprogress/mapper/TrainingProgressMapper.java
@@ -0,0 +1,9 @@
+package org.springblade.modules.trainingprogress.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springblade.modules.trainingprogress.pojo.entity.TrainingProgressEntity;
+
+@Mapper
+public interface TrainingProgressMapper extends BaseMapper<TrainingProgressEntity> {
+}
