bb8e8046c65a09144ee35f00ea0b2f21551098ac
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-27 22:24:43 +0800

    feat: add recommendation feedback mapper

---
+++ src/main/java/org/springblade/modules/recommendfeedback/mapper/RecommendFeedbackMapper.java
@@ -0,0 +1,9 @@
+package org.springblade.modules.recommendfeedback.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springblade.modules.recommendfeedback.pojo.entity.RecommendFeedbackEntity;
+
+@Mapper
+public interface RecommendFeedbackMapper extends BaseMapper<RecommendFeedbackEntity> {
+}
