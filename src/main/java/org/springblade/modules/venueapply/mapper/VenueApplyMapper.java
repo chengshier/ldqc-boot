2b0da70345c13b67c2d0aa55d4b089b7fd27b7e6
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-27 21:44:50 +0800

    feat: add venue application mapper

---
+++ src/main/java/org/springblade/modules/venueapply/mapper/VenueApplyMapper.java
@@ -0,0 +1,9 @@
+package org.springblade.modules.venueapply.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springblade.modules.venueapply.pojo.entity.VenueApplyEntity;
+
+@Mapper
+public interface VenueApplyMapper extends BaseMapper<VenueApplyEntity> {
+}
